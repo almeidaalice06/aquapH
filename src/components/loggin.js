e6b345c74d41a6451a6b6eb1fa55f80c439fa5cf
almeidaalice06 <alice.natalie@aluno.ifsp.edu.br>
2024-07-30 18:04:14 -0300

    inserindo componente de login

---
+++ src/components/loggin.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loggin() {
+  return (
+    <View>
+      <Text>Tela de loggin!</Text>
+    </View>
+  );
+}
